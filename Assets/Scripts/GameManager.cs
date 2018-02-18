53e58ca127b9eced3e696485a284c7b68faaa20f
MJMortimer <matthew.mortimer1@gmail.com>
2018-02-18 22:25:09 +1300

    Configurable stealing and stacking

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,6 +14,7 @@ public class GameManager : MonoBehaviour
     private Color _pink;
     private Color _blue;
     private Color _gridColor;
+    private IList<Color> _playerColours;
 
     private Tile[,] _board;
 
@@ -25,6 +26,8 @@ public class GameManager : MonoBehaviour
 
     private int _winLength;
     private int _playerCount;
+    private bool _stacking;
+    private bool _stealing;
 
     // UI components
 
@@ -57,6 +60,8 @@ public class GameManager : MonoBehaviour
     //Temporary setting changes
     private int? _desiredWinLength;
     private int? _desiredPlayerCount;
+    private bool? _desiredStacking;
+    private bool? _desiredStealing;
 
     void Start()
     {
@@ -136,6 +141,38 @@ public class GameManager : MonoBehaviour
             PlayerPrefs.SetInt("playerCount", _playerCount);
             playerCountButton.GetComponent<Image>().sprite = TwoPlayer;
         }
+
+        // Stacking button
+        var stackingButton = _menuUi.Find("Stacking/StackingButton").GetComponent<Button>();
+        if (PlayerPrefs.HasKey("stacking"))
+        {
+            _stacking = PlayerPrefs.GetInt("stacking") == 1;
+            stackingButton.transform.Find("StackingButtonText").GetComponent<Text>().text = _stacking 
+                ? string.Format("<color=#{0}>YEAH!</color>", ColorUtility.ToHtmlStringRGBA(_red)) 
+                : string.Format("<color=#{0}>NAH..</color>", ColorUtility.ToHtmlStringRGBA(_yellow));
+        }
+        else
+        {
+            _stacking = true;
+            PlayerPrefs.SetInt("stacking", 1);
+            stackingButton.transform.Find("StackingButtonText").GetComponent<Text>().text = string.Format("<color=#{0}>YEAH!</color>", ColorUtility.ToHtmlStringRGBA(_red));
+        }
+
+        // Stealing button
+        var stealingButton = _menuUi.Find("Stealing/StealingButton").GetComponent<Button>();
+        if (PlayerPrefs.HasKey("stealing"))
+        {
+            _stealing = PlayerPrefs.GetInt("stealing") == 1;
+            stealingButton.transform.Find("StealingButtonText").GetComponent<Text>().text = _stealing
+                ? string.Format("<color=#{0}>YEAH!</color>", ColorUtility.ToHtmlStringRGBA(_red))
+                : string.Format("<color=#{0}>NAH..</color>", ColorUtility.ToHtmlStringRGBA(_yellow));
+        }
+        else
+        {
+            _stealing = true;
+            PlayerPrefs.SetInt("stealing", 1);
+            stealingButton.transform.Find("StealingButtonText").GetComponent<Text>().text = string.Format("<color=#{0}>YEAH!</color>", ColorUtility.ToHtmlStringRGBA(_red));
+        }
     }
 
     private void FetchUiComponents()
@@ -156,6 +193,8 @@ public class GameManager : MonoBehaviour
         ColorUtility.TryParseHtmlString("#D490D0FF", out _pink);
         ColorUtility.TryParseHtmlString("#389DDAFF", out _blue);
         ColorUtility.TryParseHtmlString("#0000003D", out _gridColor);
+
+        _playerColours = new List<Color>{_red, _yellow, _pink, _blue};
 
         // Red always starts
         _activeColor = _red;
@@ -303,36 +342,42 @@ public class GameManager : MonoBehaviour
             var tile = closestCollider.GetComponent<Tile>();
             var tileText = tile.transform.Find("tileText").GetComponent<TextMesh>();
 
-            if (colisionObjectRender.color.Equals(_activeColor)) // Increase tile stack count
+            if (_stacking && colisionObjectRender.color.Equals(_activeColor)) // Increase tile stack count if stacking enabled
             {
                 tile.Stack += 1;
                 tileText.text = tile.Stack.ToString();
             }
-            else if (tile.Stack.HasValue && tile.Stack.Value > 1) // Decrease tile stack
+            else if (_stacking && tile.Stack.HasValue && tile.Stack.Value > 1) // Decrease tile stack if stacking enabled
             {
                 tile.Stack -= 1;
                 tileText.text = tile.Stack.ToString();
             }
-            else // Swap colour. Set stack count to 1
+            else // Swap colour if stealing enabled. Set stack count to 1 if stacking enabled
             {
-                //Alter the animation curve so that we fade from the color it is the color we want
-                var a = closestCollider.GetComponent<Animation>();
-                var existingClipLength = a.clip.length;
+                if (_stealing || !_playerColours.Any(it => it.Equals(colisionObjectRender.color)))
+                {
+                    //Alter the animation curve so that we fade from the color it is the color we want
+                    var a = closestCollider.GetComponent<Animation>();
+                    var existingClipLength = a.clip.length;
 
-                var rCurve = AnimationCurve.EaseInOut(0, colisionObjectRender.color.r, existingClipLength, _activeColor.r);
-                var gCurve = AnimationCurve.EaseInOut(0, colisionObjectRender.color.g, existingClipLength, _activeColor.g);
-                var bCurve = AnimationCurve.EaseInOut(0, colisionObjectRender.color.b, existingClipLength, _activeColor.b);
-                var aCurve = AnimationCurve.EaseInOut(0, colisionObjectRender.color.a, existingClipLength, _activeColor.a);
+                    var rCurve = AnimationCurve.EaseInOut(0, colisionObjectRender.color.r, existingClipLength, _activeColor.r);
+                    var gCurve = AnimationCurve.EaseInOut(0, colisionObjectRender.color.g, existingClipLength, _activeColor.g);
+                    var bCurve = AnimationCurve.EaseInOut(0, colisionObjectRender.color.b, existingClipLength, _activeColor.b);
+                    var aCurve = AnimationCurve.EaseInOut(0, colisionObjectRender.color.a, existingClipLength, _activeColor.a);
 
-                a.clip.SetCurve("", typeof(SpriteRenderer), "m_Color.r", rCurve);
-                a.clip.SetCurve("", typeof(SpriteRenderer), "m_Color.g", gCurve);
-                a.clip.SetCurve("", typeof(SpriteRenderer), "m_Color.b", bCurve);
-                a.clip.SetCurve("", typeof(SpriteRenderer), "m_Color.a", aCurve);
+                    a.clip.SetCurve("", typeof(SpriteRenderer), "m_Color.r", rCurve);
+                    a.clip.SetCurve("", typeof(SpriteRenderer), "m_Color.g", gCurve);
+                    a.clip.SetCurve("", typeof(SpriteRenderer), "m_Color.b", bCurve);
+                    a.clip.SetCurve("", typeof(SpriteRenderer), "m_Color.a", aCurve);
 
-                a.Play();
+                    a.Play();
+                }
 
-                tile.Stack = 1;
-                tileText.text = tile.Stack.ToString();
+                if (_stacking)
+                {
+                    tile.Stack = 1;
+                    tileText.text = tile.Stack.ToString();
+                }
             }
 
             // Remember the change
@@ -380,9 +425,7 @@ public class GameManager : MonoBehaviour
 
     private Color NextColor()
     {
-        var players = new List<Color> {_red, _yellow, _pink, _blue};
-
-        var activeIndex = players.IndexOf(_activeColor);
+        var activeIndex = _playerColours.IndexOf(_activeColor);
         var next = activeIndex + 1;
 
         if (next >= _playerCount)
@@ -390,7 +433,7 @@ public class GameManager : MonoBehaviour
             next = 0;
         }
 
-        return players[next];
+        return _playerColours[next];
     }
 
     public void Restart()
@@ -416,16 +459,32 @@ public class GameManager : MonoBehaviour
         {
             _winLength = _desiredWinLength.Value;
             PlayerPrefs.SetInt("winlength", _desiredWinLength.Value);
-            _requiresReset = true;
             _desiredWinLength = null;
+            _requiresReset = true;
         }
 
         if (_desiredPlayerCount.HasValue)
         {
             _playerCount = _desiredPlayerCount.Value;
             PlayerPrefs.SetInt("playerCount", _desiredPlayerCount.Value);
-            _requiresReset = true;
             _desiredPlayerCount = null;
+            _requiresReset = true;
+        }
+
+        if (_desiredStacking.HasValue)
+        {
+            _stacking = _desiredStacking.Value;
+            PlayerPrefs.SetInt("stacking", _desiredStacking.Value ? 1 : 0);
+            _desiredStacking = null;
+            _requiresReset = true;
+        }
+
+        if (_desiredStealing.HasValue)
+        {
+            _stealing = _desiredStealing.Value;
+            PlayerPrefs.SetInt("stealing", _desiredStealing.Value ? 1 : 0);
+            _desiredStealing = null;
+            _requiresReset = true;
         }
     }
 
@@ -480,6 +539,50 @@ public class GameManager : MonoBehaviour
         if (_desiredPlayerCount.HasValue && _desiredPlayerCount.Value == _playerCount)
         {
             _desiredPlayerCount = null;
+        }
+    }
+
+    public void ChangeStacking()
+    {
+        var buttonText = _menuUi.Find("Stacking/StackingButton/StackingButtonText").GetComponent<Text>();
+        var value = _desiredStacking ?? _stacking;
+
+        if (value)
+        {
+            _desiredStacking = false;
+            buttonText.text = string.Format("<color=#{0}>NAH..</color>", ColorUtility.ToHtmlStringRGBA(_yellow));
+        }
+        else
+        {
+            _desiredStacking = true;
+            buttonText.text = string.Format("<color=#{0}>YEAH!</color>", ColorUtility.ToHtmlStringRGBA(_red));
+        }
+
+        if (_desiredStacking == _stacking)
+        {
+            _desiredStacking = null;
+        }
+    }
+
+    public void ChangeStealing()
+    {
+        var buttonText = _menuUi.Find("Stealing/StealingButton/StealingButtonText").GetComponent<Text>();
+        var value = _desiredStealing ?? _stealing;
+
+        if (value)
+        {
+            _desiredStealing = false;
+            buttonText.text = string.Format("<color=#{0}>NAH..</color>", ColorUtility.ToHtmlStringRGBA(_yellow));
+        }
+        else
+        {
+            _desiredStealing = true;
+            buttonText.text = string.Format("<color=#{0}>YEAH!</color>", ColorUtility.ToHtmlStringRGBA(_red));
+        }
+
+        if (_desiredStealing == _stealing)
+        {
+            _desiredStealing = null;
         }
     }
 }
